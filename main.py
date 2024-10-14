a5d166c2dfaf8b5b842bca033dd1ab36883beb65
Kelian Massa <kelian@isaziconsulting.co.za>
2024-10-14 16:47:14 +0200

    Function play with AI

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import coloredlogs
 
 from othello import OthelloState
 from policy_iter import PolicyIteration
+from nnet import AlphaZeroSimpleCNN
+from play import Play
+
 log = logging.getLogger(__name__)
 
 coloredlogs.install(level='INFO')  # Change this to DEBUG to see more info.
@@ -16,7 +19,7 @@ args = dotdict({
     'batchSize': 32,
     'epochs': 75,
     'learningRate': 0.001,
-    'numPolicyIters': 10,
+    'numPolicyIters': 1,
     'numEpsSp': 50,
     'numEpsPit': 30,
     'numExploreSteps': 3,
@@ -24,14 +27,20 @@ args = dotdict({
     'cpuct': 1.414,
     'numItersForTrainHist': 20,
     'winThresh': 0.55,
-    'checkpointDir': 'checkpoints'
+    'checkpointDir': 'checkpoints',
+    'loadNnet': False
 })
 
 if __name__ == '__main__':
     board_size = 4
     log.info(f'Playing Othello with board size {board_size}')
     model = OthelloState(board_size)
-    nnet = PolicyIteration(
-        model,
-        args
-    )
+    if args.loadNnet:
+        nnet = AlphaZeroSimpleCNN(board_size, num_actions=board_size**2)
+        nnet.load_checkpoint(folder=args.checkpointDir, filename='best.pth.tar')
+    else:
+        nnet = PolicyIteration(
+            model,
+            args
+        )
+    Play(model, nnet, args)
